5635ef5f7edc3754d757e1210c0c460fe4a190f5
Ruslan Gumirov <gumirus29@gmail.com>
2022-12-27 21:44:02 +0500

    React.memo

--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,45 @@
+import React, { useEffect, useState, useCallback } from "react";
+import PropTypes from "prop-types";
+
+const LofOutButton = ({ onLogOut }) => {
+    useEffect(() => {
+        console.log("render button");
+    });
+
+    return (
+        <button className="btn btn-primary" onClick={onLogOut}>
+            LogOutButton
+        </button>
+    );
+};
+LofOutButton.propTypes = {
+    onLogOut: PropTypes.func
+};
+function areEqual(prevState, nextState) {
+    if (prevState.onLogOut !== nextState.onLogOut) {
+        return false;
+    }
+    return true;
+}
+
+const MemoizedLogOutButton = React.memo(LofOutButton, areEqual);
+
 const MemoWithUseCallbackExample = (props) => {
-    return null;
+    const [state, setState] = useState(false);
+    const handleLogOut = useCallback(() => {
+        localStorage.removeItem("auth");
+    }, [props]);
+    return (
+        <>
+            <button
+                className="btn btn-primary"
+                onClick={() => setState(!state)}
+            >
+                initiate render
+            </button>
+            <MemoizedLogOutButton onLogOut={handleLogOut} />
+        </>
+    );
 };
 
 export default MemoWithUseCallbackExample;

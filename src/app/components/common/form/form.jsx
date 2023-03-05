f34e3509e68beaf6f3bfe8da103bbc4c06064681
Ruslan Gumirov <gumirus29@gmail.com>
2023-03-05 12:43:59 +0500

    обрабатываем children

--- src/app/components/common/form/form.jsx
+++ src/app/components/common/form/form.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { validator } from "../../../utils/validator";
 import { PropTypes } from "prop-types";
-const FormComponent = ({ children }) => {
-    return <form>{children}</form>;
+const FormComponent = ({ children, validatorConfig }) => {
+    const [data] = useState({});
+    const [, setErrors] = useState({});
+    // const handleChange = (target) => {
+    //     setData((prevState) => ({
+    //         ...prevState,
+    //         [target.name]: target.value
+    //     }));
+    // };
+    // const isValid = Object.keys(errors).length === 0;
+    useEffect(() => validate(), [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const clonedElements = React.Children.map(children, (child) => {
+        console.log(child);
+        return child;
+    });
+    return <form>{clonedElements}</form>;
 };
 FormComponent.propTypes = {
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.npde
-    ])
+    ]),
+    validatorConfig: PropTypes.object
 };
 
 export default FormComponent;
